8fc174d460496f96ef15fb6be2b36167deacda33
Ben Judson <ben@saharagray.com>
2014-08-06 17:58:32 -0500

    slight data cleanup, update color scales, deal with undefined data values

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-//globals
+// Heavily based on:
+// http://bl.ocks.org/rgdonohue/9280446
+
 var width, height, projection, path, graticule, svg, attributeArray = [], currentAttribute = 0, playing = false;
 
 function init() {
@@ -8,38 +10,37 @@ function init() {
 
 function setMap() {
 
-  width = 1000, height = 650;  // map width and height, matches 
+  width = 1000, height = 650;
 
-  projection = d3.geo.mercator()   // define our projection with parameters
+  projection = d3.geo.mercator()
     .scale((width + 1) / 2 / Math.PI)
     .translate([width / 2, height / 2])
     .precision(.1);
 
-  path = d3.geo.path()  // create path generator function
-      .projection(projection);  // add our define projection to it
+  path = d3.geo.path()
+      .projection(projection);
 
-  graticule = d3.geo.graticule(); // create a graticule
+  graticule = d3.geo.graticule();
 
-  svg = d3.select("#map").append("svg")   // append a svg to our html div to hold our map
+  svg = d3.select("#map").append("svg")
       .attr("width", width)
       .attr("height", height);
 
-  svg.append("defs").append("path")   // prepare some svg for outer container of svg elements
+  svg.append("defs").append("path")
       .datum({type: "Sphere"})
       .attr("id", "sphere")
       .attr("d", path);
 
-  svg.append("use")   // use that svg to style with css
+  svg.append("use")
       .attr("class", "stroke")
       .attr("xlink:href", "#sphere");
 
-  svg.append("path")    // use path generator to draw a graticule
+  svg.append("path")
       .datum(graticule)
       .attr("class", "graticule")
       .attr("d", path);
 
-  loadData();  // let's load our data next
-
+  loadData();
 }
 
 function loadData() {
@@ -55,16 +56,16 @@ function processData(error,world,countryData) {
   // function accepts any errors from the queue function as first argument, then
   // each data object in the order of chained defer() methods above
 
-  var countries = world.objects.countries.geometries;  // store the path in variable for ease
-  for (var i in countries) {    // for each geometry object
-    for (var j in countryData) {  // for each row in the CSV
-      if(countries[i].properties.id == countryData[j].id) {   // if they match
-        for(var k in countryData[i]) {   // for each column in the a row within the CSV
-          if(k != 'name' && k != 'id') {  // let's not add the name or id as props since we already have them
+  var countries = world.objects.countries.geometries;
+  for (var i in countries) {
+    for (var j in countryData) {
+      if(countries[i].properties.id == countryData[j].id) {
+        for(var k in countryData[i]) {
+          if(k != 'name' && k != 'id') {
             if(attributeArray.indexOf(k) == -1) { 
-               attributeArray.push(k);  // add new column headings to our array for later
+               attributeArray.push(k);
             }
-            countries[i].properties[k] = Number(countryData[j][k])  // add each CSV column key/value to geometry object
+            countries[i].properties[k] = Number(countryData[j][k]);
           } 
         }
         break;
@@ -76,34 +77,38 @@ function processData(error,world,countryData) {
 
 function drawMap(world) {
 
-    svg.selectAll(".country")   // select country objects (which don't exist yet)
-      .data(topojson.feature(world, world.objects.countries).features)  // bind data to these non-existent objects
-      .enter().append("path") // prepare data to be appended to paths
-      .attr("class", "country") // give them a class for styling and access later
-      .attr("id", function(d) { return "code_" + d.properties.id; }, true)  // give each a unique id for access later
-      .attr("d", path); // create them using the svg path generator defined above
+    svg.selectAll(".country")
+      .data(topojson.feature(world, world.objects.countries).features)
+      .enter().append("path")
+      .attr("class", "country")
+      .attr("id", function(d) { return "code_" + d.properties.id; }, true)
+      .attr("d", path);
 
     var dataRange = getDataRange();
     d3.selectAll('.country')
     .attr('fill-opacity', function(d) {
-        return getColor(d.properties[attributeArray[currentAttribute]], dataRange);  // give them an opacity value based on their current value
+        return getColor(d.properties[attributeArray[currentAttribute]], dataRange);
     });
 }
 
 function sequenceMap() {
   
-    var dataRange = getDataRange(); // get the min/max values from the current year's range of data values
-    d3.selectAll('.country').transition()  //select all the countries and prepare for a transition to new values
-      .duration(750)  // give it a smooth time period for the transition
+    var dataRange = getDataRange();
+    d3.selectAll('.country').transition() 
+      .duration(750) 
       .attr('fill-opacity', function(d) {
-        return getColor(d.properties[attributeArray[currentAttribute]], dataRange, attributeArray[currentAttribute]);  // the end color value
+        return getColor(d.properties[attributeArray[currentAttribute]], dataRange, attributeArray[currentAttribute]);
       })
 
 }
 
 function getColor(valueIn, valuesIn, attribute) {
   var reverse = ['pol-rights', 'civil-libs'],
-      range = reverse.indexOf(attribute) > -1 ? [1,.3] : [.3,1];
+      range = reverse.indexOf(attribute) > -1 ? [1,.2] : [.2,1];
+
+  if(typeof valueIn === 'undefined'){
+    return 0.05;
+  }
 
   var color = d3.scale.linear()
     .domain([valuesIn[0],valuesIn[1]])
